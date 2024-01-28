c46c56788dd4507c5611dbd078e1848ccf196385
FraidoonOmarzai <fraidoon_omarzai@yahoo.com>
2024-01-28 19:42:41 +0000

    template.py, env, requirements.txt, setup.py, logging and utils done

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # end-to-end-SMS-Spam-classifier
+
+## Steps
+
+- git clone the repo from github
+
+- define the template of the project
+
+- define setup.py
+
+- create env
+
+```bash
+conda create -n sms-env python=3.9 -y
+conda activate sms-env
+```
+
+- define requirements.txt
+
+```bash
+pip install -r requirements.txt
+```
+
+- define logging and utils
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+__version__ = "0.0.1"
+
+SRC_REPO = "SMSClassifier"
+AUTHOR_USER_NAME = "Techie"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    description="implementing complete ml project life-cycle",
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages()  # working in windows
+)
